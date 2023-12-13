660767e88d661fbfb068b911101226c933fbe253
jeevitha-reddy <jeevithareddy24@gmail.com>
2023-12-13 14:34:06 +0530

    added second script

--- 03.variables.sh
+++ 03.variables.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-PERSON1=Ramu
-PERSON2=Somu
+Date=$(date)
 
-echo "$PERSON1: Hello $PERSON2, Good Morning"
-echo "$PERSON2: Hi $PERSON1, very Good Morning"
-echo "$PERSON1: How are you $PERSON2?"
-echo "$PERSON2: I am good $PERSON1, How are you"
+echo " Date and time is :${date} "
+
