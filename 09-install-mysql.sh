81431cbb728544a5f6059789cd9daf6b4e5480c5
jeevitha-reddy <jeevithareddy24@gmail.com>
2023-12-13 21:09:43 +0530

    added second script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -5,10 +5,18 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 
 then
-	echo" Error::Please run this script with root user"
+	echo "Error::Please run this script with root user"
 exit 1
 else 
 	echo "you are in root user"
-exit 1
+
 fi
 yum install mysql -y
+
+if [$? -ne 0 ]
+then
+	echo "ERROR:: installing mysql is failed"
+	exit 1
+else
+	echo "Installed mysql successfully"
+fi
