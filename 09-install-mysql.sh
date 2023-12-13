dcd9344e6bfe7c6a44fc5411f0813a717ecdd4c2
jeevitha-reddy <jeevithareddy24@gmail.com>
2023-12-13 21:00:54 +0530

    added second script

---
+++ 09-install-mysql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [$ID -ne 0 ]
+
+then
+	echo" Error::Please run this script with root user"
+#exit 1
+else 
+	echo "you are in root user"
+#exit 1
+fi
+yum install mysql -y
