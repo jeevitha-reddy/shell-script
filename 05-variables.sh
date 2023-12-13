eaf1afde2a6f3ffe9fde313d4e6462480b2d0e68
jeevitha-reddy <jeevithareddy24@gmail.com>
2023-12-13 14:19:25 +0530

    added second script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo"Please enter your username:: "
+
+read -s USERNAME #the value u enter automatically attached to file
+
+
+echo "Please enter your Password:: "
+read -s PASSWORD
+
+echo "Username is: $USERNAME, password is:$PASSWORD" # printing for validation 
