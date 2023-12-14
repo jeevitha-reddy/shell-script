2e8cae5bf8481260fe52f476a6026b760947b012
jeevitha-reddy <jeevithareddy24@gmail.com>
2023-12-14 19:14:54 +0530

    added validation

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 TIMESTAMP=$(date +%f-%H-%M-%S)
 LOG_FILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "secipt started executing at $TIMESTAMP" &>> LOG_FILE
+echo "secipt started executing at $TIMESTAMP" &>> $LOG_FILE
 
 VALIDATE(){
 	if [ $1 -ne 0 ]
@@ -31,7 +31,7 @@ fi
 
 for package in $@
 do
-	yum list installed $package &>> LOG_FILE #check installed or not 
+	yum list installed $package &>> $LOG_FILE #check installed or not 
 	if [ $? -ne 0 ]
 		then
 			yum install $package -y
