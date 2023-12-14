cd3df481fad919463fdd05ee97d443abbc08367b
jeevitha-reddy <jeevithareddy24@gmail.com>
2023-12-14 15:18:51 +0530

    added validation

--- 11-logs.sh
+++ 11-logs.sh
@@ -3,9 +3,9 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
+#R="\e[31m"
+#G="\e[32m"
+#N="\e[0m"
 
 LOG_FILE="/temp/$0-$TIMESTAMP.log"
 
@@ -14,10 +14,10 @@ echo "Script started executing at $TIMESTAMP" &>> $LOG_FILE
 VALIDATE(){
 	if [ $1 -ne 0 ]
 then
-	echo "ERROR:: $2 ...$R failed $N"
+	echo "ERROR:: $2 ... failed"
 	exit 1
 else
-	echo "Installed  $2 ... $G successfully $N"
+	echo "Installed  $2 ... successfully"
 fi
 }
 
