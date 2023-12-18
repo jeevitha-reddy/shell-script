714c586c56e2f34a781cf1cdd9dc2c5f5532e68d
jeevitha-reddy <jeevithareddy24@gmail.com>
2023-12-18 21:28:35 +0530

    new roboshop shell script projects

---
+++ ifs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+file=/etc/passwd
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -f $file ] #denotes opposite
+then
+	echo -e "$R source directory : $SOURCE_DIR does not exists. $N"
+
+fi
+
+while IFS=":" read -r username password user_id group_id user-fullname home_dir shell_path
+do 
+	echo "username: $username"
+	echo "user ID:  $user_id"
+	echo "Group ID: $group_id"
+
+done < $file
