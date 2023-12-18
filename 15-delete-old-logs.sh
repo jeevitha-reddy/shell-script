053f45e9c64cfea3acf1e99bd17e8c3aa94348e8
jeevitha-reddy <jeevithareddy24@gmail.com>
2023-12-18 20:39:26 +0530

    new roboshop shell script projects

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+
+SOURCE_DIR="/tmp/shellscript-logs"
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-SOURCE_DIR="/tmp/shellscript-logs"
-
 if [ ! -d $SOURCE_DIR ] # ! denots the opposite
 then
-	echo -e "$R source directory : $SOURCE_DIR does not exists"
+	echo -e "$R source directory : $SOURCE_DIR does not exists. $N"
 fi
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
