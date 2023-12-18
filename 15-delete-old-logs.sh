11b46eab66fa51f14a20e9a206cf8f5b629b3116
jeevitha-reddy <jeevithareddy24@gmail.com>
2023-12-18 20:42:51 +0530

    new roboshop shell script projects

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -15,7 +15,7 @@ fi
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
-while IFS=read -r line # internal field separator
+while IFS=read -r line  # internal field separator
 do
- echo -e"Deleting file :$line"
+ echo "Deleting file :$line"
 done<<< $FILES_TO_DELETE # command outputdata giving input
